1e33ae89f632347f8ffac16179ba031ba88caada
Viktorya <Viktorya@DESKTOP-AHJPH50>
2024-03-18 23:50:21 +0400

    Add project files.

---
+++ Library/DataBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace Library
+{
+    internal class DataBase
+    {
+        // SqlConnection object to manage the connection to the database
+        private SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-AHJPH50\SQLEXPRESS01; Initial Catalog=Library; Integrated Security=True");
+
+        // Method to open the database connection
+        public void openConnection()
+        {
+            try
+            {
+                if (sqlConnection.State == System.Data.ConnectionState.Closed)
+                {
+                    sqlConnection.Open();
+                    Console.WriteLine("Connection opened successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error opening connection: " + ex.Message);
+            }
+        }
+
+        // Method to close the database connection
+        public void closeConnection()
+        {
+            try
+            {
+                if (sqlConnection.State == System.Data.ConnectionState.Open)
+                {
+                    sqlConnection.Close();
+                    Console.WriteLine("Connection closed successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error closing connection: " + ex.Message);
+            }
+        }
+
+        public SqlConnection getSqlConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
---
+++ Library/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class Form1 : Form
+    {
+        DataBase database = new DataBase();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
